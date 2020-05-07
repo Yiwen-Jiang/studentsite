7b63cd2948b77fac346bd11d20bc2ac7650cadaa
Yiwen-Jiang <59891508+Yiwen-Jiang@users.noreply.github.com>
2020-05-07 06:59:03 -1000

    updated!

--- art128/final/js/script.js
+++ art128/final/js/script.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // TOGGLE NAV 
-    var mymenubutton = document.querySelector('.menu-button');
-    var mytogglenav = document.querySelector('.toggle-nav');
 
-    mymenubutton.addEventListener('click', function () {
-        mytogglenav.classList.toggle('expanded');
-    });
+    // // TOGGLE NAV 
+    // var mymenubutton = document.querySelector('.menu-button');
+    // var mytogglenav = document.querySelector('.toggle-nav');
 
-    //STICKY NAV - REMOVE EXPANDED CLASS FOR MOBILE
-    var stickynavlinks = document.querySelectorAll(".sticky nav a");
-    for (var i = 0; i < stickynavlinks.length; i++) {
-        stickynavlinks[i].onclick = function () {
-            mytogglenav.classList.remove("expanded");
-        }
-    };
+    // mymenubutton.addEventListener('click', function () {
+    //     mytogglenav.classList.toggle('expanded');
+    // });
+
+    // //STICKY NAV - REMOVE EXPANDED CLASS FOR MOBILE
+    // var stickynavlinks = document.querySelectorAll(".sticky nav a");
+    // for (var i = 0; i < stickynavlinks.length; i++) {
+    //     stickynavlinks[i].onclick = function () {
+    //         mytogglenav.classList.remove("expanded");
+    //     }
+    // };
 
 });
